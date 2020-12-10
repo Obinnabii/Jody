f63c89124211de5126b6f6bb62eb0124d2665d45
Obi Abii <coa22@cornell.edu>
2020-12-10 13:19:30 -0500

    fixed authors.mli

--- authors.mli
+++ authors.mli
@@ -1,7 +1,7 @@
 (**
    JoDy is a minimum viable product for a language that has automatic memoization,
-   allowing dynamic programming algorithms to be written using recursion, but 
-   actually cache results under the hood.
+   allowing dynamic programming algorithms to be written using recursion, by 
+   actually cacheing results under the hood.
 
    This is Project built on JoCalf by Obi Abii and Jack Nash
 

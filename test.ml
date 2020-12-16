55f319f875a43835359f915960833383235bff93
Obi Abii <coa22@cornell.edu>
2020-12-15 21:20:25 -0500

    add more reflective tests

--- test.ml
+++ test.ml
@@ -9,13 +9,8 @@ open Main
 let str_max_int = string_of_int max_int
 let str_min_int = string_of_int min_int
 
-(* OCaml allows {|...|} as a syntax for strings in which the ...
-   can contain unescaped quotes.  This is super useful for
-   constructing test cases, as shown below. *)
 let tests = [
-  (* All of these tests will currently fail because you have not yet
-     implemented interpretation of any of these syntactic forms. A
-     completed interpreter should pass all of them, though. *)
+  (* These tests show some examples of JoDy Syntax *)
   "int constant", {|42|}, "42";
   "negative int constant", {|-1|}, "-1";
   "true", {|true|}, "true";
@@ -24,6 +19,13 @@ let tests = [
   "if truthy", {|if true then 5 else 6|}, "5";
   "if falsey", {|if 0 then 3110 else 4110|}, "4110";
   "sequence", {|5+5;10+10|}, "20";
+  "let", {|let x = 5 in x + 2|}, "7";
+  "#1", {|let x = [5,6] in x.#1 + 2|}, "7";
+  "#2", {|let x = [5,6] in x.#2 + 2|}, "8";
+  "function app",{|(fun(a) -> a+ 2) 4|}, "6";
+  "function app",{|let x = (fun(a b) -> a+ b) in x 3 9|}, "12";
+  "rec",{|let rec x(a) = if a<=1 then a else a * x (a-1) in x 4|}, "24";
+  "dyn",{|let dyn x(a) = if a<=1 then a else a * x (a-1) in x 4|}, "24";
 ]
 
 let make_interp_expr_test n in_str out_str =
